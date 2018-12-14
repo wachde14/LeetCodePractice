641debeace49f8d4a109fad4b7386ce03342a8c9
wachde14 <41805653+wachde14@users.noreply.github.com>
2018-12-14 10:56:59 -0700

    Add AddTwoNumbersWithLinkedLists

---
+++ Medium Questions/DataStructures/ListNode.cs
@@ -0,0 +1,9 @@
+﻿namespace Medium_Questions.DataStructures
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+}
